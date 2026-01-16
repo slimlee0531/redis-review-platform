97e0ce149e805d7d38d1e3d4e90bf14d5c0d3322
slimlee0531 <slimlee0531@163.com>
2026-01-16 18:40:23 +0800

    refactor:lua脚本解决命令原子性问题

--- src/main/java/com/review/utils/SimpleRedisLock.java
+++ src/main/java/com/review/utils/SimpleRedisLock.java
@@ -1,8 +1,11 @@
 package com.review.utils;
 
 import cn.hutool.core.lang.UUID;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
 
+import java.util.Collections;
 import java.util.concurrent.TimeUnit;
 
 public class SimpleRedisLock implements ILock {
@@ -10,15 +13,22 @@ public class SimpleRedisLock implements ILock {
     private final StringRedisTemplate stringRedisTemplate;
     private final String name;
 
-    // 集群模式下区分服务器
-    private static final String NODE_ID_PREFIX = UUID.randomUUID().toString(true) + "-";
-
     public SimpleRedisLock(StringRedisTemplate stringRedisTemplate, String name) {
         this.stringRedisTemplate = stringRedisTemplate;
         this.name = name;
     }
 
     private static final String KEY_PREFIX = "lock:";
+    // 集群模式下区分服务器
+    private static final String NODE_ID_PREFIX = UUID.randomUUID().toString(true) + "-";
+
+    // lua 脚本调用
+    private static final DefaultRedisScript<Long> UNLOCK_SCRIPT;
+    static {
+        UNLOCK_SCRIPT = new DefaultRedisScript<>();
+        UNLOCK_SCRIPT.setLocation(new ClassPathResource("unlock.lua"));
+        UNLOCK_SCRIPT.setResultType(Long.class);
+    }
 
     @Override
     public boolean tryLock(long timeoutSec) {
@@ -33,6 +43,16 @@ public class SimpleRedisLock implements ILock {
 
     @Override
     public void unlock() {
+        // 调用 lua 脚本
+        stringRedisTemplate.execute(
+                UNLOCK_SCRIPT,
+                Collections.singletonList(KEY_PREFIX + name),
+                NODE_ID_PREFIX + Thread.currentThread().getId()
+        );
+    }
+
+/*    @Override
+    public void unlock() {
         // 获取线程标识 (区分服务器、线程)
         String threadId = NODE_ID_PREFIX + Thread.currentThread().getId();
         // 获取锁中的标识
@@ -41,6 +61,6 @@ public class SimpleRedisLock implements ILock {
         if (threadId.equals(id)) {
             stringRedisTemplate.delete(KEY_PREFIX + name);
         }
-    }
+    }*/
 
 }
