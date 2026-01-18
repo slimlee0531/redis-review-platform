b44574e5c57a25783a53af5b287b591e3a2f4b81
slimlee0531 <slimlee0531@163.com>
2026-01-18 16:13:35 +0800

    feat:查看探店笔记

--- src/main/java/com/review/service/IBlogService.java
+++ src/main/java/com/review/service/IBlogService.java
@@ -1,16 +1,12 @@
 package com.review.service;
 
+import com.review.dto.Result;
 import com.review.entity.Blog;
 import com.baomidou.mybatisplus.extension.service.IService;
 
-/**
- * <p>
- *  服务类
- * </p>
- *
- * @author 虎哥
- * @since 2021-12-22
- */
 public interface IBlogService extends IService<Blog> {
 
+    Result queryHotBlog(Integer current);
+
+    Result queryBlogById(Long id);
 }
--- src/main/java/com/review/service/impl/BlogServiceImpl.java
+++ src/main/java/com/review/service/impl/BlogServiceImpl.java
@@ -1,20 +1,65 @@
 package com.review.service.impl;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.review.dto.Result;
 import com.review.entity.Blog;
+import com.review.entity.User;
 import com.review.mapper.BlogMapper;
 import com.review.service.IBlogService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.review.service.IUserService;
+import com.review.utils.SystemConstants;
 import org.springframework.stereotype.Service;
 
-/**
- * <p>
- *  服务实现类
- * </p>
- *
- * @author 虎哥
- * @since 2021-12-22
- */
+import javax.annotation.Resource;
+import java.io.Serializable;
+import java.util.List;
+
 @Service
 public class BlogServiceImpl extends ServiceImpl<BlogMapper, Blog> implements IBlogService {
+
+    @Resource
+    private IUserService userService;
+
+    /**
+     * 热门博客
+     * @param current
+     * @return
+     */
+    public Result queryHotBlog(Integer current) {
+        // 根据用户查询
+        Page<Blog> page = query()
+                .orderByDesc("liked")
+                .page(new Page<>(current, SystemConstants.MAX_PAGE_SIZE));
+        // 获取当前页数据
+        List<Blog> records = page.getRecords();
+        // 查询用户
+        records.forEach(this::queryBlogUser);
+        return Result.ok(records);
+    }
+
+    /**
+     * 根据id查询博客
+     * @param id
+     * @return
+     */
+    public Result queryBlogById(Long id) {
+        // 1. 查询 blog
+        Blog blog = getById(id);
+        if (blog == null) {
+            return Result.fail("笔记不存在！！");
+        }
+        // 2. 查询 blog 有关的用户
+        queryBlogUser(blog);
+
+        return Result.ok(blog);
+    }
+
+    private void queryBlogUser(Blog blog) {
+        Long userId = blog.getUserId();
+        User user = userService.getById(userId);
+        blog.setName(user.getNickName());
+        blog.setIcon(user.getIcon());
+    }
 
 }
